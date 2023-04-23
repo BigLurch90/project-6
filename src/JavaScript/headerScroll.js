23d8da34f348d74ad7d08367f1743aff9aa1a0ab
Jonas Johansson <jonas.johansson@studerande.karlskogafhs.se>
2023-04-23 22:49:26 +0200

    last sec update

--- src/JavaScript/headerScroll.js
+++ src/JavaScript/headerScroll.js
@@ -1,14 +1,11 @@
 const headarScroll = () => {
     
-    // Get element from DOM
     const header = document.querySelector('.site-header')
     
 
-    // Toggle Header Class
     const toggleHeaderClass = () => {
         
-        // If window has scrolled mor then 200px, add a class
-        // else remove it
+
         if ( window.scrollY > 70 ) {
             header.classList.add('site-header--scroll')
         } else {
@@ -16,7 +13,6 @@ const headarScroll = () => {
         }
     }
 
-    // Add event to window scroll
     window.addEventListener('scroll', toggleHeaderClass)
 
 }
